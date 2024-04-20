c26ff9608832ff3813028c8f8fa5528ef691d00c
Yassir Karroum <ukarroum17@gmail.com>
2024-04-20 10:21:13 +0200

    Added WebSourceType and WebSourceRoot

--- braum.py
+++ braum.py
@@ -1,8 +1,26 @@
 import argparse
+from dataclasses import dataclass
+from pathlib import Path
+from enum import Enum
+
+
+class WebSourceType(Enum):
+    DJANGO = 1
+
+
+@dataclass
+class WebSourceRoot:
+    root: Path
+    type: WebSourceType
+
+
+def scan_source():
+    pass
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Braum: An experimental web ids")
-    parser.add_argument("--log-path", help="path to the access logs")
+    parser = argparse.ArgumentParser("Braum: An experimental web vulnerability scanner tool")
+    parser.add_argument("--src-path", help="path to the source logs")
+    parser.add_argument("--hostname", help="path to the hostname")
 
     args = parser.parse_args()
-
