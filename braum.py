cb2b6c8f97bc90bd395a5d135ce0d6b49c8db098
Yassir Karroum <ukarroum17@gmail.com>
2024-04-24 17:46:48 +0200

    Removed source code related code

--- braum.py
+++ braum.py
@@ -1,26 +1,13 @@
 import argparse
-from dataclasses import dataclass
-from pathlib import Path
-from enum import Enum
 
-
-class WebSourceType(Enum):
-    DJANGO = 1
-
-
-@dataclass
-class WebSourceRoot:
-    root: Path
-    type: WebSourceType
-
-
-def scan_source():
-    pass
-
+scans = []
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Braum: An experimental web vulnerability scanner tool")
-    parser.add_argument("--src-path", help="path to the source logs")
     parser.add_argument("--hostname", help="path to the hostname")
 
     args = parser.parse_args()
+
+    for scan in scans:
+        if scan(args.hostname):
+            print(f"Found vulnerability: {scan.name}")
